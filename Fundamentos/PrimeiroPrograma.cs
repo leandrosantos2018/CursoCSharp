34a62fbd15fc6d041b0f84bc41125e23ba24eb0b
leandro <leleuramos@gmail.com>
2021-05-24 23:50:59 -0300

    Adicionar arquivos de projeto.

---
+++ Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace CursoCSharp.Fundamentos
+{
+    class PrimeiroPrograma
+    {
+       public static void Executar()
+        {
+            Console.Write("primeiro");
+            Console.WriteLine("programa");
+            Console.WriteLine("terminou");
+
+        }
+    }
+}
